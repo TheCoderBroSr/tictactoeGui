679b34938d65fc39c4e2f94f16a3a06b9440294b
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-06-20 13:05:21 +0530

    changed background color

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ SCREEN_HEIGHT = 600
 WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(("TicTacToe GUI"))
 
+BLACK = (5, 0, 20)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                end()
+
+    WIN.fill(BLACK)
+
+    pygame.display.update()
         
